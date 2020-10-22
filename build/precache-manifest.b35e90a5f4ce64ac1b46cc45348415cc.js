1f8adfbee5f781c05620f745dbaada550655dee3
NAIMYS <naimy.soukaina@gmail.com>
2020-10-22 10:57:17 +0100

    update

---
+++ build/precache-manifest.b35e90a5f4ce64ac1b46cc45348415cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f155f7b88f594696cfc3",
+    "url": "/portfolio-naimy-cv/static/js/runtime~main.f155f7b8.js"
+  },
+  {
+    "revision": "856be52e72be5284777e",
+    "url": "/portfolio-naimy-cv/static/js/main.856be52e.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio-naimy-cv/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "856be52e72be5284777e",
+    "url": "/portfolio-naimy-cv/static/css/main.605651a1.chunk.css"
+  },
+  {
+    "revision": "a3c7a788f27f93cfee523058561a79c3",
+    "url": "/portfolio-naimy-cv/index.html"
+  }
+];
